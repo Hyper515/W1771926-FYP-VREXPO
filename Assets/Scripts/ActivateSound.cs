06392d45a7c240dc42e6123b9b5d7614c67d056e
Ethan Akin <129392892+Hyper515@users.noreply.github.com>
2024-04-03 23:31:07 +0100

    Reverted Audio Script
    Brought back Original Audio Source script that allows user to interact and activate audio (Issue - Audio is played when orbs are activated)

--- Assets/Scripts/ActivateSound.cs
+++ Assets/Scripts/ActivateSound.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class ActivateSound : MonoBehaviour
 {
     public AudioSource audioSource;
-    private bool userInteracted = false;
 
-    // Use OnTriggerEnter for detecting when something enters the trigger zone
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        // Check if the object interacting with this one has the tag "Player" and the user hasn't interacted yet
-        if (other.CompareTag("Player") && !userInteracted)
+        // Check if Fire1 input or primary button on the Oculus controller is pressed
+        if (Input.GetButtonDown("Fire1") || Input.GetAxis("Fire1") > 0)
         {
-            // Set the flag to true to indicate user interaction
-            userInteracted = true;
             // If the audio source is not playing, start playing it
             if (!audioSource.isPlaying)
             {
