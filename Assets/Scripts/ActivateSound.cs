a4fec09979d28a70826b5528496d1824dfe1a72b
Ethan Akin <129392892+Hyper515@users.noreply.github.com>
2024-04-03 22:32:11 +0100

    Updating Audio File

--- Assets/Scripts/ActivateSound.cs
+++ Assets/Scripts/ActivateSound.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class ActivateSound : MonoBehaviour
 {
     public AudioSource audioSource;
+    private bool userInteracted = false;
 
     // Use OnTriggerEnter for detecting when something enters the trigger zone
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object interacting with this one has the tag "Player"
-        if (other.CompareTag("Player"))
+        // Check if the object interacting with this one has the tag "Player" and the user hasn't interacted yet
+        if (other.CompareTag("Player") && !userInteracted)
         {
+            // Set the flag to true to indicate user interaction
+            userInteracted = true;
             // If the audio source is not playing, start playing it
             if (!audioSource.isPlaying)
             {
